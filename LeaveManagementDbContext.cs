a11fecc0cd2596f934fd0592f4bbf487887e7ac6
Dhanuvrat Gupta <53273058+DhanuvratGupta@users.noreply.github.com>
2024-06-13 10:28:54 +0530

    Add files via upload
    A .NET MVC Leave Management System with REST API facilitates efficient leave request handling. It uses .NET MVC for web application development and employs a RESTful API for seamless integration and data exchange. This system allows employees to submit leave requests, enables supervisors to approve or reject them, and provides administrators with oversight and reporting capabilities

---
+++ LeaveManagementDbContext.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Leave_Mangement
+{
+    public class LeaveManagementDbContext
+    {
+        private readonly IConfiguration _config;
+
+        public LeaveManagementDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public MySqlConnection OpenConnection()
+        {
+            string connectionString = _config.GetSection("connectionString").Value;
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
